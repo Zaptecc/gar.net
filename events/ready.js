1c4b76abd375b8470e3148dfc160fd7435488f61
Michael Wilke <enarchtechnologies@gmail.com>
2017-07-24 00:40:30 -0500

    Create ready.js (fs)

---
+++ events/ready.js
@@ -0,0 +1,7 @@
+module.exports = async bot => {
+    await wait(1000);
+    //bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "RDY");
+    //you define the bot.log command in function.js, might as well us it
+    bot.log("log", `I'm ready!`, "RDY");
+    bot.user.setGame("PUT GAME HERE");
+}
