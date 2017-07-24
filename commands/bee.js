62e7d7f57754968029664f52eaec7443887de3da
Michael Wilke <enarchtechnologies@gmail.com>
2017-07-24 01:04:39 -0500

    Create bee.js

---
+++ commands/bee.js
@@ -0,0 +1,19 @@
+exports.run = async(bot, message, args, level) => {
+    msg.channel.send("According to all known laws of aviation, there is no known way a bee should be able to fly. Its wings are to small too get its fat little body off the ground. The bee, of course, flies anyways because bees don't care what humans think is impossible.")
+		msg.channel.send("Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little.")
+		msg.channel.send("Barry! Breakfast is ready!")
+		msg.channel.send("Coming! Hang on a second. Hello?")
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'bee',
+    description: 'Simple test to see if I am responding!',
+    usage: 'bee'
+};
