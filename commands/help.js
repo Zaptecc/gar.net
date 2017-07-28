41de158f7f8e34cbc52ea7908f53967c41a77d3a
lightningdude38 <garnett.strack@gmail.com>
2017-07-28 01:23:32 -0500

    changed ping and help

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,37 @@
 exports.run = async(bot, message, args, level) => {
-  msg.channel.send('uhhhhhhhhh...');
+      const Discord = require('discord.js');
+    if (!args[0]) {
+        const myCommands = bot.commands.filter(c => c.conf.permLevel <= level);
+        var helpbox = new Discord.RichEmbed();
+        helpbox.setTitle("Command List")
+               .setDescription(`Use ${bot.config.prefix}help <commandname> for details`)
+               .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+        myCommands.forEach(c => {
+            helpbox.addField(c.help.name, c.help.description)
+        }
+    );
+        message.channel.send({embed: helpbox});
+    } else {
+        let command = '';
+        if (bot.commands.has(args[0])) {
+            command = bot.commands.get(args[0]);
+        } else if (bot.aliases.has(args[0])) {
+            command = bot.commands.get(bot.aliases.get(args[0]));
+        };
+        if (!command) return message.reply(`That command \`${args[0]}\` doesn't seem to exist, nor is it an alias. Try again!`);
+        if (bot.commands.has(command)) {
+            command = bot.commands.get(command);
+            var helpCommand = new Discord.RichEmbed();
+            helpCommand.setTitle(command.help.name)
+            .addField('Description', `${command.help.description}`)
+            .addField('Usage', `${bot.config.prefix}${command.help.usage}`)
+            .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+            if (command.conf.aliases != "") {
+                helpCommand.addField('Aliases', `${command.conf.aliases.join(', ')}`)
+            }
+            message.channel.send({embed: helpCommand});
+        };
+    };
 };
 
 exports.conf = {
--- commands/testhelp.js
+++ commands/testhelp.js
@@ -1,5 +1,33 @@
 exports.run = async(bot, message, args, level) => {
-  //codehere
+  msg.channel.send({embed: {
+    	color: 3447003,
+    	author: {
+      		name: bot.user.username + ' help',
+      		icon_url: bot.user.avatarURL
+    	},
+    	title: "This is an embed",
+    	url: "http://google.com",
+    	description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      		},
+      		{
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      		},
+      		{
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: bot.user.avatarURL,
+      text: "© Example"
+    		}
+  		}
+	});
 };
 
 exports.conf = {
