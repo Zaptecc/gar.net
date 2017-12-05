bf13cd90d5d6111cd157d57efaf523bdafb3c12e
Garnett Strack <garnett.strack@gmail.com>
2017-12-05 17:59:12 -0600

    Beta v0.03 *fix*
    oops that shouldnt be there :stuck_out_tongue:

--- bot.js
+++ bot.js
@@ -9,21 +9,6 @@ require("./modules/functions.js")(bot);
 bot.commands = new discord.Collection();
 bot.aliases = new discord.Collection();
 
-
-function play(connection, message) {
-    var server = servers[message.guild.id]
-
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}))
-
-    server.queue.shift()
-
-    server.dispatcher.on("end", function() {
-        if (server.queue[0]) play(connection, message)
-        else connection.disconnect()
-    })
-}
-
-
 (async function() {
 
     const commandFiles = await readdir('./commands/');
