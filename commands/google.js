7a1caebae91e520b133263137c83645c085812a8
Garnett Strack <garnett.strack@gmail.com>
2018-02-27 08:47:44 -0600

    google?

---
+++ commands/google.js
@@ -0,0 +1,53 @@
+module.exports = (bot, message) => {
+
+    bot.checkForUpvote(message).then(res => {
+        if (res) {
+            google.build({
+                q: message.content,
+                num: 1,
+            }, (err, response) => {
+                console.log(err);
+                console.log(response);
+                if (err) {
+                    console.error(err);
+                    message.channel.send('ERROR: Search failed');
+                    return;
+                }
+                if (response.totalResults === 0 || response.items === undefined) {
+                    message.channel.send('No results.');
+                } else if (response.items !== undefined) {
+                    console.log(response);
+                    var results = new Discord.RichEmbed();
+                    var link = response.items[0].link;
+                    var title = response.items[0].title;
+                    var desc = response.items[0].snippet;
+                    results.setAuthor('Google', 'https://maxcdn.icons8.com/Share/icon/Logos/google_logo1600.png', 'https://www.google.com/')
+                        .setTitle(title)
+                        .setURL(link)
+                        .setDescription(desc + ' [more](' + link + ')')
+                        .setTimestamp()
+                        .setColor(message.guild.me.displayColor)
+                        .setFooter('Powered by RoBot', bot.user.avatarURL);
+                    message.channel.send({ embed: results });
+                }
+            });
+        } else {
+            bot.promptForUpvote(message, this.name);
+        }
+});
+
+}
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+	name: 'google',
+	description: 'Google search things!',
+	usage: 'google [search query]'
+};
